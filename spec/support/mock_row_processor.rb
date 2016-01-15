dc4826c1d9478580f7df0b9c3c19b3db647d70c0
Tom Collier <collier@apartmentlist.com>
2016-01-14 22:02:28 -0800

    Remove unused mocking code

--- spec/support/mock_row_processor.rb
+++ spec/support/mock_row_processor.rb
@@ -1,25 +1,12 @@
 class MockRowProcessor
-  attr_reader :record, :messages
+  attr_reader :messages
 
   def initialize(record, payload:)
-    @record = record
-    @success = false
-    @messages = []
   end
 
   def process!
-    case record['name']
-    when 'Rex'
-      @success = true
-    when 'Human'
-      raise 'bad human'
-    else
-      @success = false
-      @messages << 'bad dog'
-    end
   end
 
   def success?
-    @success
   end
 end
