cb9852b89c3c7d8e1bed580c9166e6d14935884f
Tom Collier <collier@apartmentlist.com>
2016-01-15 14:10:08 -0800

    Add spec for Handling results

--- spec/bulk_processor/csv_processor_spec.rb
+++ spec/bulk_processor/csv_processor_spec.rb
@@ -20,12 +20,12 @@ describe BulkProcessor::CSVProcessor do
     let(:row_processor_1) do
       instance_double(BulkProcessor::Role::RowProcessor, process!: true,
                                                          success?: true,
-                                                         messages: [])
+                                                         messages: ['Woah 1'])
     end
     let(:row_processor_2) do
       instance_double(BulkProcessor::Role::RowProcessor, process!: true,
                                                          success?: true,
-                                                         messages: [])
+                                                         messages: ['Woah 2'])
     end
 
     before do
@@ -42,6 +42,71 @@ describe BulkProcessor::CSVProcessor do
       expect(row_processor_1).to receive(:process!)
       expect(row_processor_2).to receive(:process!)
       subject.start
+    end
+
+    context 'handling the results' do
+      let(:handler) do
+        instance_double(BulkProcessor::Role::Handler, complete!: true,
+                                                      fail!: true)
+      end
+
+      before do
+        allow(MockHandler).to receive(:new).and_return(handler)
+        allow(row_processor_2).to receive(:success?).and_return(false)
+      end
+
+      it 'initializes the handler with the payload' do
+        expect(MockHandler).to receive(:new)
+          .with(payload: payload, successes: anything, errors: anything)
+          .and_return(handler)
+        subject.start
+      end
+
+      it 'initializes the handler with successes' do
+        expected_successes = { 0 => ['Woah 1'] }
+        expect(MockHandler).to receive(:new)
+          .with(payload: anything, successes: expected_successes, errors: anything)
+          .and_return(handler)
+        subject.start
+      end
+
+      it 'initializes the handler with errors' do
+        expected_errors = { 1 => ['Woah 2'] }
+        expect(MockHandler).to receive(:new)
+          .with(payload: anything, successes: anything, errors: expected_errors)
+          .and_return(handler)
+        subject.start
+      end
+
+      it 'sends a completion message the handler' do
+        expect(handler).to receive(:complete!)
+        subject.start
+      end
+
+      context 'when processing an item raises an unrescued error' do
+        before do
+          allow(row_processor_1).to receive(:process!)
+            .and_raise(StandardError, 'Broken')
+        end
+
+        it 'sends a failure message the handler' do
+          expect(handler).to receive(:fail!).with(instance_of(StandardError))
+          subject.start
+        end
+      end
+
+      context 'when processing an item raises a SIGTERM' do
+        before do
+          allow(row_processor_1).to receive(:process!)
+            .and_raise(SignalException, 'SIGTERM')
+        end
+
+        it 're-raises the error' do
+          expect do
+            subject.start
+          end.to raise_error(SignalException, 'SIGTERM')
+        end
+      end
     end
   end
 end
