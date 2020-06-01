4c2e96ead45b9d3df786710194377b4fa117afcb
jeffnoehren <jnoehren@apartmentlist.com>
2020-06-01 14:26:49 -0700

    [SUPLY-1170] - adding gcp pods to the backend

---
+++ lib/bulk_processor/back_end/gcp_pods/split_csv_task.rb
@@ -0,0 +1,27 @@
+require 'rake'
+
+class BulkProcessor
+  module BackEnd
+    class GcpPods
+      class SplitCSVTask
+        include Rake::DSL
+
+        def install_task
+          namespace :bulk_processor do
+            desc 'Split a CSV file and process each piece'
+            task :split, [:processor_class, :payload, :key, :num_chunks] => :environment do |_task, args|
+              BulkProcessor::SplitCSV.new(
+                args[:processor_class].constantize,
+                PayloadSerializer.deserialize(args[:payload]),
+                args[:key],
+                args[:num_chunks].to_i
+              ).perform
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+BulkProcessor::BackEnd::GcpPods::SplitCSVTask.new.install_task
