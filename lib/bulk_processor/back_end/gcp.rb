b7103d0f550371f8fe1a4582ebd44484c362b345
jeffnoehren <jnoehren@apartmentlist.com>
2020-06-10 15:25:51 -0700

    adding new requirements

--- lib/bulk_processor/back_end/gcp.rb
+++ lib/bulk_processor/back_end/gcp.rb
@@ -7,6 +7,9 @@ class BulkProcessor
         @processor_class = processor_class.name
         @payload = PayloadSerializer.serialize(payload)
         @key = key
+
+        GcpManager.add_task('start-bulk-processor', 'rake bulk_processor_gcp_pods:start')
+        GcpManager.add_task('split-bulk-processor', 'rake bulk_processor_gcp_pods:split')
       end
 
       def start
